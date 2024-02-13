b150edb98868f35bc1701aeb4315b6eab11a442b
Constantin Hager <constantin.hager@outlook.de>
2024-02-13 23:48:38 +0100

    Initial Commit of Lab3

--- Labs/Lab3_AL.ps1
+++ Labs/Lab3_AL.ps1
@@ -1,16 +1,19 @@
 <#
 .SYNOPSIS
-    This Script will deploy a SQL Server AG with the help of DSC.
+    This Script will deploy a SQL Server AG in fully AL Fashion.
 .DESCRIPTION
-    This Script will deploy a SQL Server AG with the help of DSC.
+    This Script will deploy a SQL Server AG in fully AL Fashion.
 
     The following VMs will be deployed:
-    - Lab2DC
-    - Lab2SQL1
-    - Lab2SQL2
+    - Lab3DC
+    - Lab3SQL1
+    - Lab3SQL2
 
-    Both VMs will be installed with SQL Server 2022 Enterpise Edition with DSC.
-    A SQL Server AG (Always-On Availibility Group) will be deployed with DSC.
+    Both VMs will be installed with SQL Server 2022 Enterpise Edition with AL.
+    A SQL Server AG (Always-On Availibility Group) will be deployed with AL.
+
+    The full sql server feature command line documentation can be found here:
+    https://learn.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-ver16#Feature
 .EXAMPLE
     Test-MyTestFunction -Verbose
     Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
@@ -31,13 +34,21 @@ $PSDefaultParameterValues = @{
 
 $splat = @{
     Name             = 'NATSwitchLab3'
-    HyperVProperties = @{ SwitchType = 'Internal'; AdapterName = 'vEthernet (NATSwitchLab2)' }
+    HyperVProperties = @{ SwitchType = 'Internal'; AdapterName = 'vEthernet (NATSwitchLab3)' }
     AddressSpace     = '192.168.3.0/24'
 }
 Add-LabVirtualNetworkDefinition @splat
 
 # Domain Controller
-$netAdapter = New-LabNetworkAdapterDefinition -VirtualSwitch 'NATSwitchLab3' -UseDhcp -MacAddress '0017fb000005'
+#$netAdapter = New-LabNetworkAdapterDefinition -VirtualSwitch 'NATSwitchLab3' -UseDhcp -MacAddress '0017fb000005'
+$splat = @{
+    VirtualSwitch  = 'NATSwitchLab3'
+    Ipv4Address    = '192.168.3.10'
+    Ipv4Gateway    = '192.168.3.1'
+    Ipv4DNSServers = '192.168.3.10', '168.63.129.16'
+}
+$netAdapter = New-LabNetworkAdapterDefinition @splat
+
 $splat = @{
     Name            = 'LAB3DC'
     OperatingSystem = 'Windows Server 2022 Datacenter'
@@ -48,14 +59,63 @@ $splat = @{
 }
 Add-LabMachineDefinition @splat
 
-# SQL Server 1
-$netAdapter = New-LabNetworkAdapterDefinition -VirtualSwitch 'NATSwitchLab3' -UseDhcp -MacAddress '0017fb00000d'
-Add-LabMachineDefinition -Name LAB3SQL1 -Processors 2 -NetworkAdapter $netAdapter -Roles SQLServer2022
 
-# SQL Server 2
-$netAdapter = New-LabNetworkAdapterDefinition -VirtualSwitch 'NATSwitchLab3' -UseDhcp -MacAddress '0017fb00000e'
-Add-LabMachineDefinition -Name LAB3SQL2 -Processors 2 -NetworkAdapter $netAdapter -Roles SQLServer2022
+# Configure AL Cluster Role
+$clusterRole = Get-LabMachineRoleDefinition -Role FailoverNode -Properties @{
+    ClusterName = 'LAB3SQLCL'
+    ClusterIP   = '192.168.3.200'
+}
+$SqlRole = Get-LabMachineRoleDefinition -Role SQLServer2022 -Properties @{
+    Features              = 'SQLEngine,Tools'
+    SQLSvcAccount         = 'contoso\sqlsvc'
+    SQLSvcPassword        = 'SomePass1'
+    AgtSvcAccount         = 'contoso\sqlsvc'
+    AgtSvcPassword        = 'SomePass1'
+    AgtSvcStartupType     = 'Automatic'
+    BrowserSvcStartupType = 'Automatic'
+}
+$roles = @()
+$roles += $clusterRole
+$roles += $SqlRole
+
+# SQL Server 1, Failover Cluster Node
+$splat = @{
+    VirtualSwitch  = 'NATSwitchLab3'
+    Ipv4Address    = '192.168.3.11'
+    Ipv4Gateway    = '192.168.3.1'
+    Ipv4DNSServers = '192.168.3.10', '168.63.129.16'
+}
+$netAdapter = New-LabNetworkAdapterDefinition @splat
+Add-LabDiskDefinition -Name Lab3SQL1DataDrive1 -DiskSizeInGb 100
+Add-LabDiskDefinition -Name Lab3SQL1DataDrive2 -DiskSizeInGb 100
+$splat = @{
+    Name           = 'LAB3SQL1'
+    Processors     = 2
+    NetworkAdapter = $netAdapter
+    Roles          = $roles
+    DiskName       = 'Lab3SQL1DataDrive1', 'Lab3SQL1DataDrive2'
+}
+Add-LabMachineDefinition @splat
+
+# SQL Server 2, Failover Cluster Node
+$splat = @{
+    VirtualSwitch  = 'NATSwitchLab3'
+    Ipv4Address    = '192.168.3.12'
+    Ipv4Gateway    = '192.168.3.1'
+    Ipv4DNSServers = '192.168.3.10', '168.63.129.16'
+}
+$netAdapter = New-LabNetworkAdapterDefinition @splat
+Add-LabDiskDefinition -Name Lab3SQL2DataDrive1 -DiskSizeInGb 100
+Add-LabDiskDefinition -Name Lab3SQL2DataDrive2 -DiskSizeInGb 100
+$splat = @{
+    Name           = 'LAB3SQL2'
+    Processors     = 2
+    NetworkAdapter = $netAdapter
+    Roles          = $roles
+    DiskName       = 'Lab3SQL2DataDrive1', 'Lab3SQL2DataDrive2'
+}
+Add-LabMachineDefinition @splat
 
 Install-Lab
 
-Show-LabDeploymentSummary
+Show-LabDeploymentSummary -Detailed
